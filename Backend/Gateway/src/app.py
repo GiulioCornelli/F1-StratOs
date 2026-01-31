e0e59b8a9e61c0f7b0bb2284ab1b59d7978f0e29
GiulioCornelli <cornelli.giulio05@gmail.com>
2026-01-31 11:13:05 +0100

    inizializazione ambiente frontend

--- Backend/Gateway/src/app.py
+++ Backend/Gateway/src/app.py
@@ -7,21 +7,14 @@ from contextlib import asynccontextmanager
 
 
 # librerie custom
-<<<<<<< HEAD
-from .routes import routerDriver, routerMeeting
-from src.logo import GATEWAY_BANNER,F1_STRATOS_EXIT,BOLD, RESET, GREEN
-=======
 from .routes import routerDriver
 from src.logo import GATEWAY_BANNER,F1_STRATOS_EXIT
->>>>>>> main
 
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print(GATEWAY_BANNER)
-    
-    
     yield
     print(F1_STRATOS_EXIT)
 
